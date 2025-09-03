116816c74d6e990f4aa1b652e53ac2760a715a14
Hernán E. Noriega <hernan.noriega@outlook.es>
2025-09-03 11:04:27 -0600

    UPDATE

---
+++ folps/__init__.py
@@ -0,0 +1,5 @@
+# FOLPS package
+from .folps import *
+from .cosmo_class import *
+from .tools import *
+from .tools_jax import *
--- setup.py
+++ setup.py
@@ -2,21 +2,34 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-
 package_basename = 'folps'
 package_dir = os.path.join(os.path.dirname(__file__), package_basename)
 sys.path.insert(0, package_dir)
 import _version
 version = _version.__version__
 
+# Read long description from README.md
+with open('README.md', encoding='utf-8') as f:
+      long_description = f.read()
 
-setup(name=package_basename,
+setup(
+      name=package_basename,
       version=version,
       author='Hernan E. Noriega',
       author_email='',
       description='FOLPS code (includes JAX)',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       license='BSD3',
-      url='http://github.com/cosmodesi/folpsax',
+      url='https://github.com/henoriega/FOLPSpipe',
       install_requires=['numpy', 'scipy', 'jax', 'interpax'],
       extras_require={},
-      packages=find_packages())
+      packages=find_packages(),
+      include_package_data=True,
+      python_requires='>=3.7',
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+      ],
+)
